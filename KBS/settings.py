f532c265322ee81a20694de4865abb8963e93a4b
toa.pita <toa.pita@gmail.com>
2022-12-13 08:50:43 -0700

    adding files for hosting

--- KBS/settings.py
+++ KBS/settings.py
@@ -16,15 +16,17 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jj8oy7$$s-mg+132=zi!wmoltnz1zhu$)e01*lqs=c)nnpwmx+'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
