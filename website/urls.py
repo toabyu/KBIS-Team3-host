4966c4c14a995289313d626ba27b063787fd8edc
toa.pita <toa.pita@gmail.com>
2022-12-12 18:17:05 -0700

    changes for login
    This is a bunch of changes for login routes

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,11 @@
 # Project Description: A useful website to serve as a class reference for students in the core.
 
 from django.urls import path
-from .views import index, about, content, viewClass, viewArticle, addArticle, editArticle, manageArticles, manageClassArticles, addClassArticle
+from .views import index, about, content, viewClass, viewArticle, addArticle, editArticle, manageArticles, manageClassArticles, addClassArticle,login
 # Here are all the paths. See the views folder for specifics
 urlpatterns = [
     path("", index, name="Index"),
+    path("login",login,name="login"),
     path("about", about, name="About"),
     path("content", content, name="Content"),
     path("content/<str:classID>", viewClass, name="ClassContent"),
--- website/views.py
+++ website/views.py
@@ -2,11 +2,45 @@
 # Section: 003
 # Project Description: A useful website to serve as a class reference for students in the core.
 
+# shortcut login
+username = "is403"
+password = "python"
+authUser = False
 
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Author, Article, Paragraph
 # Create your views here.
+
+def login(request):
+    # let the user login
+    global authUser
+    if request.method == "POST":
+        if request.POST["uname"].lower() == username.lower() and request.POST["pwd"] == password:
+            # if it is then login and return manage page
+            authUser = True
+            return manageArticles(request)
+        else:
+            # otherwise leave them logged out
+            authUser = False
+            # return the login page
+            return loginView(request)
+    else:
+        return loginView(request)
+
+# login route
+def loginView(request):
+    # login route
+    return render(request,"website/login.html")
+
+
+def logout(request):
+    # set authUser to false
+    global authUser
+    authUser = False
+    # render the login page
+    return render(request,"website/login.html")
+
 
 # Home page
 def index(request):
@@ -55,148 +89,162 @@ def viewArticle(request, articleID):
 
 # page to allow auth users to add articles
 def addArticle(request):
-    # This allows us to use the same route (get and post). 
-    # Get will give them the page
-    # Post will post the article to the DB and return the class page
-    if request.method == "GET":
-        #This page will allow an authorized user to create a new article
-        return render(request, 'website/addArticle.html')
-    else:
-        # If it isn't get it must be post.
-        # grab first and last name
-        fName = request.POST["first_name"]
-        lName = request.POST["last_name"]
-        # check if the author already exists
-        if len(Author.objects.filter(first_name=fName, last_name=lName)):
-            # if it does grab the author
-            author = Author.objects.get(first_name=fName, last_name = lName)
+    if authUser:
+        # This allows us to use the same route (get and post). 
+        # Get will give them the page
+        # Post will post the article to the DB and return the class page
+        if request.method == "GET":
+            #This page will allow an authorized user to create a new article
+            return render(request, 'website/addArticle.html')
         else:
-            # if it doesn't exist create a new author
-            author = Author()
-            # set new author first and last names
-            author.first_name = fName.lower()
-            author.last_name = lName.lower()
-            author.save()
-        # make a new article
-        article = Article()
-        article.author = author
-        article.title = request.POST["title"].lower()
-        article.description = request.POST["description"]
-        article.classID = request.POST["classID"].upper()
-        # save the article
-        article.save()
-        # split up the text by paragraph.
-        array = request.POST["content"].split("\n")
-        for i in array:
-            # loop through each paragraph
-            # add paragraph
-            p = Paragraph()
-            # set the content to the text
-            p.content = i
-            # add the paragraph to the article(by setting the id)
-            p.article = article
-            # save it
-            p.save()
-        # save the article
-        article.save()
-        # return the article viewArticle
-        return viewArticle(request,article.id)
+            # If it isn't get it must be post.
+            # grab first and last name
+            fName = request.POST["first_name"]
+            lName = request.POST["last_name"]
+            # check if the author already exists
+            if len(Author.objects.filter(first_name=fName, last_name=lName)):
+                # if it does grab the author
+                author = Author.objects.get(first_name=fName, last_name = lName)
+            else:
+                # if it doesn't exist create a new author
+                author = Author()
+                # set new author first and last names
+                author.first_name = fName.lower()
+                author.last_name = lName.lower()
+                author.save()
+            # make a new article
+            article = Article()
+            article.author = author
+            article.title = request.POST["title"].lower()
+            article.description = request.POST["description"]
+            article.classID = request.POST["classID"].upper()
+            # save the article
+            article.save()
+            # split up the text by paragraph.
+            array = request.POST["content"].split("\n")
+            for i in array:
+                # loop through each paragraph
+                # add paragraph
+                p = Paragraph()
+                # set the content to the text
+                p.content = i
+                # add the paragraph to the article(by setting the id)
+                p.article = article
+                # save it
+                p.save()
+            # save the article
+            article.save()
+            # return the article viewArticle
+            return viewArticle(request,article.id)
+    else:
+        return loginView(request)
+
 
 # Allow auth user to add a specific class related article
 def addClassArticle(request,classID):
-    #This page will allow an authorized user to create a new article with the classID preset to "+classID
-    if request.method == "GET":
-        context = {
-            "classID":classID.upper()
-        }
-        return render(request, 'website/addArticle.html',context)
+    if authUser:
+        #This page will allow an authorized user to create a new article with the classID preset to "+classID
+        if request.method == "GET":
+            context = {
+                "classID":classID.upper()
+            }
+            return render(request, 'website/addArticle.html',context)
+        else:
+            # add the article and return the page
+            return addArticle(request)
     else:
-        # add the article and return the page
-        return addArticle(request)
+        return loginView(request)
 
 # Allow auth user to edit articles
 def editArticle(request,articleID):
     # This page will allow an authorized user to edit article # "+str(articleID)
+    if authUser:
+        if request.method =="POST":
+            # save the edits
+            # grab first and last name
+            fName = request.POST["first_name"]
+            lName = request.POST["last_name"]
+            # check if the author already exists
+            if len(Author.objects.filter(first_name=fName, last_name=lName)):
+                # if it does grab the author
+                author = Author.objects.get(first_name=fName, last_name = lName)
+            else:
+                # if it doesn't exist create a new author
+                author = Author()
+                # set new author first and last names
+                author.first_name = fName.lower()
+                author.last_name = lName.lower()
+                author.save()
+            # make a new article
+            article = Article.objects.filter(id=articleID)[0]
+            article.author = author
+            article.title = request.POST["title"].lower()
+            article.description = request.POST["description"]
 
-    if request.method =="POST":
-        # save the edits
-        # grab first and last name
-        fName = request.POST["first_name"]
-        lName = request.POST["last_name"]
-        # check if the author already exists
-        if len(Author.objects.filter(first_name=fName, last_name=lName)):
-            # if it does grab the author
-            author = Author.objects.get(first_name=fName, last_name = lName)
-        else:
-            # if it doesn't exist create a new author
-            author = Author()
-            # set new author first and last names
-            author.first_name = fName.lower()
-            author.last_name = lName.lower()
-            author.save()
-        # make a new article
+            # delete all paragraphs, since we will add them all again below with the new edits
+            Paragraph.objects.filter(article=article).delete()
+
+            # split up the text by paragraph.
+            array = request.POST["content"].split("\n")
+            for i in array:
+                p = Paragraph()
+                p.content = i
+                p.article = article
+                p.save()
+            article.save()
+
         article = Article.objects.filter(id=articleID)[0]
-        article.author = author
-        article.title = request.POST["title"].lower()
-        article.description = request.POST["description"]
-
-        # delete all paragraphs, since we will add them all again below with the new edits
-        Paragraph.objects.filter(article=article).delete()
-
-        # split up the text by paragraph.
-        array = request.POST["content"].split("\n")
-        for i in array:
-            p = Paragraph()
-            p.content = i
-            p.article = article
-            p.save()
-        article.save()
-
-    article = Article.objects.filter(id=articleID)[0]
-    content = Paragraph.objects.filter(article=article)
-    
-    context = {
-        'title': f"Edit Article \"{article}\"",
-        'article': article,
-        'content': content
-    }
-    return render(request, 'website/editArticle.html', context)
-
+        content = Paragraph.objects.filter(article=article)
+        
+        context = {
+            'title': f"Edit Article \"{article}\"",
+            'article': article,
+            'content': content
+        }
+        return render(request, 'website/editArticle.html', context)
+    else:
+        return loginView(request)
 # allow auth user to view all articles with buttons to add, edit, and delete
 def manageArticles(request):
-    #This page will display a table of all the articles on the site with an 
-    # add button at the top and edit and delete buttons for each article
+    if authUser:
+        #This page will display a table of all the articles on the site with an 
+        # add button at the top and edit and delete buttons for each article
 
-    # handles deleting an article that the user chooses to delete
-    if request.method == "POST":
-        deleteArticleID = request.POST["deleteArticleID"]
-        Article.objects.filter(id=deleteArticleID).delete()
+        # handles deleting an article that the user chooses to delete
+        if request.method == "POST":
+            deleteArticleID = request.POST["deleteArticleID"]
+            Article.objects.filter(id=deleteArticleID).delete()
 
-    articles = Article.objects.all()
-    
-    context = {
-        'title': 'Manage All Articles',
-        'articles': articles
-    }
-    # return the manage page
-    return render(request, 'website/manageArticle.html', context)
+        articles = Article.objects.all()
+        
+        context = {
+            'title': 'Manage All Articles',
+            'articles': articles
+        }
+        # return the manage page
+        return render(request, 'website/manageArticle.html', context)
+    else:
+        return loginView(request)
 
 # Same thing as above but filtered to a specific class
 def manageClassArticles(request, classID):
-    # This page will display all the articles related to "+classID+" with 
-    # an add button at the top 
-    # and edit and delete buttons for each article
-    
-    # handles deleting an article that the user chooses to delete
-    if request.method == "POST":
-        deleteArticleID = request.POST["deleteArticleID"]
-        Article.objects.filter(id=deleteArticleID).delete()
+    if authUser:    
+        # This page will display all the articles related to "+classID+" with 
+        # an add button at the top 
+        # and edit and delete buttons for each article
+        
+        # handles deleting an article that the user chooses to delete
+        if request.method == "POST":
+            deleteArticleID = request.POST["deleteArticleID"]
+            Article.objects.filter(id=deleteArticleID).delete()
 
-    articles = Article.objects.all().filter(classID=classID)
-    
-    context = {
-        'title': f'Manage {classID} Articles',
-        'classID': classID,
-        'articles': articles
-    }
-    return render(request, 'website/manageArticle.html', context)
+        articles = Article.objects.all().filter(classID=classID)
+        
+        context = {
+            'title': f'Manage {classID} Articles',
+            'classID': classID,
+            'articles': articles
+        }
+        return render(request, 'website/manageArticle.html', context)
+    else:
+        return loginView(request)
